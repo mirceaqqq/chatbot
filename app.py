88cc0b57e09a9a66988eed46d9246541772dea1c
mirceaqqq <164914892+mirceaqqq@users.noreply.github.com>
2025-04-11 06:39:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from langchain_groq import ChatGroq
 
 # Load API key
 load_dotenv()
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 
 # Initialize Groq client for LangChain
 llm = ChatGroq(
